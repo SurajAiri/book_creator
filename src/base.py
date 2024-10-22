a2e6454780454a9c1b981c5360927c5e436c34c2
SurajAiri <surajairi.ml@gmail.com>
2024-10-22 13:57:42 +0530

    refactor

--- src/base.py
+++ src/base.py
@@ -56,12 +56,19 @@ class BookCreator:
             except Exception as e:
                 print(f"Invalid Json Received from LLM on attempt {retry + 1}: {e}")
                 if retry == self.BOOK_STRUCTURE_RETRY_LIMIT - 1:
-                    if progress_listener:
-                        progress_listener(f"Failed to create book structure after {self.BOOK_STRUCTURE_RETRY_LIMIT} attempts.")
+                    
+                    self.handle_progress(progress_listener,f"Failed to create book structure after {self.BOOK_STRUCTURE_RETRY_LIMIT} attempts.")
                     raise Exception("Failed to create book structure after retries.")
-                if progress_listener:
-                    progress_listener(f"Retrying book structure generation. Retry Count: {retry + 1}")
+                
+                self.handle_progress(progress_listener,f"Retrying book structure generation. Retry Count: {retry + 1}")
 
+    def handle_progress(self, progress_listener, message, progress_data=None, stop_on_flag=True):
+        if progress_listener:
+            flag = progress_listener(message, progress_data)
+            if stop_on_flag and flag:
+                exit()
+                return True
+        return False
 
     def create_book(self, book_context, book_genre, book_name, progress_listener=None):
         """
@@ -81,8 +88,7 @@ class BookCreator:
         print(f"Book Path: {book_path}")
 
         # Notify progress listener
-        if progress_listener:
-            progress_listener(f"Starting book creation: {book_name}")
+        self.handle_progress(progress_listener,f"Starting book creation: {book_name}",stop_on_flag=False)
 
         # Create directory if it doesn't exist
         if not os.path.exists(self.output_dir):
@@ -90,9 +96,8 @@ class BookCreator:
 
         # If the book file exists and the progress file does not, assume the book is fully generated
         if os.path.exists(book_path) and not os.path.exists(book_progress_path):
-            if progress_listener:
-                progress_listener(f"Book '{book_name}' already exists. No further processing is required.")
-            exit()
+            self.handle_progress(progress_listener,f"Book '{book_name}' already exists. No further processing is required.",stop_on_flag=False)
+            return
 
         # Load book structure
         if not os.path.exists(book_structure_path):
@@ -114,24 +119,21 @@ class BookCreator:
 
         # Generate the book introduction only if it hasn't been generated yet
         if not progress_data["intro_generated"]:
-            if progress_listener:
-                progress_listener("Generating book introduction.",progress_data)
+            self.handle_progress(progress_listener,"Generating book introduction.",progress_data)
             
             book_intro = generate_book_introduction(self.model, book_context, book_genre, data, self.intro_prompt)
             save_docx(book_path, book_intro.strip())
             add_page_break(book_path)
             self.update_progress(book_progress_path, progress_data, intro_generated=True)
             
-            if progress_listener:
-                progress_listener("Successfully saved book introduction.",progress_data)
+            self.handle_progress(progress_listener,"Successfully saved book introduction.",progress_data)
 
         # Loop through chapters, headings, and subheadings, saving progress after each step
         for chapter_index in range(progress_data['chapter_index'], len(data['chapters'])):
             chapter = data['chapters'][chapter_index]
             chapter_title = chapter['chapter_title']
             chapter_headings = chapter['headings']
-            if progress_listener:
-                progress_listener(f"Generating content for chapter: {chapter_title}",progress_data)
+            self.handle_progress(progress_listener,f"Generating content for chapter: {chapter_title}",progress_data)
 
             # Generate chapter introduction only if resuming from a new chapter
             if progress_data['heading_index'] == -1:
@@ -141,16 +143,15 @@ class BookCreator:
                 # Save progress after chapter intro generation
                 self.update_progress(book_progress_path, progress_data, chapter_index=chapter_index, heading_index=0)
                 
-                if progress_listener:
-                    progress_listener(f"Successfully saved chapter intro: {chapter_title}",progress_data)
+                self.handle_progress(progress_listener,f"Successfully saved chapter intro: {chapter_title}",progress_data)
 
             # Loop through headings
             for heading_index in range(progress_data['heading_index'], len(chapter_headings)):
                 heading = chapter_headings[heading_index]
                 heading_title = heading['heading']
                 sub_headings = heading['subheadings']
-                if progress_listener:
-                    progress_listener(f"Generating content for heading: {heading_title}",progress_data)
+                self.handle_progress(progress_listener,f"Generating content for heading: {heading_title}",progress_data)
+                    
 
                 if progress_data['subheading_index'] == -1:
                     heading_content = generate_book_heading(self.model, book_context, book_genre, chapter_title, heading_title, heading_index + 1, sub_headings, self.heading_prompt)
@@ -159,13 +160,13 @@ class BookCreator:
                     # Save progress after heading generation
                     self.update_progress(book_progress_path, progress_data, chapter_index=chapter_index, heading_index=heading_index, subheading_index=0)
 
-                    if progress_listener:
-                        progress_listener(f"Successfully saved heading content: {heading_title}",progress_data)
+
+                    self.handle_progress(progress_listener,f"Successfully saved heading content: {heading_title}",progress_data)
+                        
                 # Loop through subheadings
                 for subheading_index in range(progress_data['subheading_index'], len(sub_headings)):
                     sub_heading = sub_headings[subheading_index]
-                    if progress_listener:
-                        progress_listener(f"Generating content for subheading: {sub_heading}",progress_data)
+                    self.handle_progress(progress_listener,f"Generating content for subheading: {sub_heading}",progress_data)
 
                     sub_heading_content = generate_book_subheading(self.model, book_context, book_genre, chapter_title, heading_title, heading_index, sub_heading, self.subheading_prompt)
                     save_docx(book_path, sub_heading_content.strip())
@@ -173,8 +174,7 @@ class BookCreator:
                     # Update progress after subheading generation
                     self.update_progress(book_progress_path, progress_data, chapter_index=chapter_index, heading_index=heading_index, subheading_index=subheading_index + 1)
                     
-                    if progress_listener:
-                        progress_listener(f"Successfully saved subheading content: {sub_heading}",progress_data)
+                    self.handle_progress(progress_listener,f"Successfully saved subheading content: {sub_heading}",progress_data)
 
                 # Reset subheading index for the next heading
                 self.update_progress(book_progress_path, progress_data, subheading_index=-1)
@@ -189,4 +189,4 @@ class BookCreator:
         if os.path.exists(book_progress_path):
             os.remove(book_progress_path)
         if progress_listener:
-            progress_listener("Book creation process completed.\nBook saved at: " + book_path)
+            self.handle_progress(progress_listener,"Book creation process completed.\nBook saved at: " + book_path,stop_on_flag=False)
